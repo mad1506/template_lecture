11a8ed671fe5806d966bd00ecdc9472eebad0044
김진용 <brave_devil@naver.com>
2018-07-09 07:08:28 +0000

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,6 @@
+//= require Chart.min
+//= require sb-admin.min
+//= require sb-admin-datatables.min
+//= require sb-admin-charts.min
+//= require jquery.dataTables
+//= require dataTables.bootstrap4
